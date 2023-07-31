345aec76d8302df02ef5bd4ccc74f8ea90a054af
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-07-31 11:08:14 -0400

    Added parameters as Attributes instead of dynamic table

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -75,9 +75,276 @@ def main():
         ],
         groups=[
             NWBGroupSpec(
-                name='ParameterTable',
-                neurodata_type_inc='DynamicTable',
-                doc='An extension of DynamicTable that allows special treatment of tabular parameters.',
+                name='ParameterGroup',
+                neurodata_type_inc='NWBDataInterface',
+                doc='A group of all the parameters used by moseq-extract.',
+                attributes=[
+                    NWBAttributeSpec(
+                        name="angle_hampel_sig",
+                        doc="Angle filter sig",
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='angle_hampel_span',
+                        doc='Angle filter span',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_depth_range_min',
+                        doc='Minimum depth to search for floor of arena (in mm)',
+                        dtype='float32',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_depth_range_max',
+                        doc='Maximum depth to search for floor of arena (in mm)',
+                        dtype='float32',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_dilate_x',
+                        doc='Size of the mask dilation (to include environment walls) in the x dimension',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_dilate_y',
+                        doc='Size of the mask dilation (to include environment walls) in the y dimension',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_fill_holes',
+                        doc='If True, fill holes in ROI',
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_gradient_filter',
+                        doc='If True, exclude walls with gradient filtering',
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_gradient_kernel',
+                        doc='Kernel size for Sobel gradient filtering',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_gradient_threshold',
+                        doc='Gradient must be < this to include points',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_index',
+                        doc='Index of which background mask(s) to use.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_shape',
+                        doc='Shape to use for the mask dilation (ellipse or rect).',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_weight_area',
+                        doc='Feature weighting (area) of the background mask.',
+                        dtype='float64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_weight_extent',
+                        doc='Feature weighting (extent) of the background mask.',
+                        dtype='float64',
+                    ),
+                    NWBAttributeSpec(
+                        name='bg_roi_weight_dist',
+                        doc='Feature weighting (dist) of the background mask.',
+                        dtype='float64',
+                    ),
+                    NWBAttributeSpec(
+                        name='cable_filter_iters',
+                        doc='Number of cable filter iterations.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='cable_filter_shape',
+                        doc='Cable filter shape (rectangle or ellipse).',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='cable_filter_size_x',
+                        doc='Cable filter size (in pixels, x-direction).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='cable_filter_size_y',
+                        doc='Cable filter size (in pixels, x-direction).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='centroid_hampel_sig',
+                        doc='Hampel filter sig.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='centroid_hampel_span',
+                        doc='Hampel filter span.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='chunk_overlap',
+                        doc='Frames overlapped in each chunk. Useful for cable tracking.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='chunk_size',
+                        doc='Number of frames for each processing iteration.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='compress',
+                        doc='Convert .dat to .avi after successful extraction.',
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='compress_chunk_size',
+                        doc='Chunk size for .avi compression.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='compress_threads',
+                        doc='Number of threads for encoding.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='config_file',
+                        doc='Location of the config file.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='crop_size_width',
+                        doc='Width of cropped mouse image.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='crop_size_height',
+                        doc='Height of cropped mouse image.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='flip_classifier',
+                        doc='Location of the flip classifier used to properly orient the mouse (.pkl file).',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='flip_classifier_smoothing',
+                        doc='Number of frames to smooth flip classifier probabilities.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='fps',
+                        doc='Frame rate of camera.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='frame_dtype',
+                        doc='Data type for processed frames.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='frame_trim_beginning',
+                        doc='Frames to trim from beginning of data.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='frame_trim_end',
+                        doc='Frames to trim from end of data.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='max_height',
+                        doc='Max mouse height from floor (mm).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='min_height',
+                        doc='Min mouse height from floor (mm).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='model_smoothing_clips_x',
+                        doc='Model smoothing clips in the x-direction.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='model_smoothing_clips_y',
+                        doc='Model smoothing clips in the y-direction.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='output_dir',
+                        doc='Output directory to save the results h5 file.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='spatial_filter_size',
+                        doc='Space prefilter kernel (median filter, must be odd).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tail_filter_shape',
+                        doc='Tail filter shape.',
+                        dtype='text',
+                    ),
+                    NWBAttributeSpec(
+                        name='tail_filter_size_x',
+                        doc='Tail filter size in the x-direction.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tail_filter_size_y',
+                        doc='Tail filter size in the y-direction.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='temporal_filter_size',
+                        doc='Time prefilter kernel (median filter, must be odd).',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tracking_model_init',
+                        doc='Method for tracking model initialization.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tracking_model_ll_clip',
+                        doc='Clip log-likelihoods below this value.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tracking_model_ll_threshold',
+                        doc='Threshold on log-likelihood for pixels to use for update during tracking.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tracking_model_ll_threshold',
+                        doc='Threshold on log-likelihood to include pixels for centroid and angle calculation.',
+                        dtype='int64',
+                    ),
+                    NWBAttributeSpec(
+                        name='tracking_model_segment',
+                        doc='If True, segment likelihood mask from tracking model.',
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='use_plane_bground',
+                        doc="If True, Use a plane fit for the background. Useful for mice that don't move much.",
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='use_tracking_model',
+                        doc="Use an expectation-maximization style model to aid mouse tracking. Useful for data with cables.",
+                        dtype='bool',
+                    ),
+                    NWBAttributeSpec(
+                        name='write_movie',
+                        doc="Write a results output movie including an extracted mouse.",
+                        dtype='bool',
+                    ),
+                ]
             ),
             NWBGroupSpec(
                 name='flipped_series',
